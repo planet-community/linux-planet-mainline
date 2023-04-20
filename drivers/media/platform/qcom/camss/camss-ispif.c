ed8c7483a83bce26571d39807f429521411271b5
Otto Pflüger <otto.pflueger@abscue.de>
2023-04-20 17:53:07 +0200

    WIP: camss support for MSM8917
    Signed-off-by: Otto Pflüger <otto.pflueger@abscue.de>

--- drivers/media/platform/qcom/camss/camss-ispif.c
+++ drivers/media/platform/qcom/camss/camss-ispif.c
@@ -1111,6 +1111,8 @@ int msm_ispif_subdev_init(struct camss *camss,
 	/* Number of ISPIF lines - same as number of CSID hardware modules */
 	if (camss->version == CAMSS_8x16)
 		ispif->line_num = 2;
+	else if (camss->version == CAMSS_8x17)
+		ispif->line_num = 3;
 	else if (camss->version == CAMSS_8x96 ||
 		 camss->version == CAMSS_660)
 		ispif->line_num = 4;
@@ -1126,7 +1128,8 @@ int msm_ispif_subdev_init(struct camss *camss,
 		ispif->line[i].ispif = ispif;
 		ispif->line[i].id = i;
 
-		if (camss->version == CAMSS_8x16) {
+		if (camss->version == CAMSS_8x16 ||
+		    camss->version == CAMSS_8x17) {
 			ispif->line[i].formats = ispif_formats_8x16;
 			ispif->line[i].nformats =
 					ARRAY_SIZE(ispif_formats_8x16);
@@ -1162,7 +1165,8 @@ int msm_ispif_subdev_init(struct camss *camss,
 	if (camss->version == CAMSS_8x16)
 		ret = devm_request_irq(dev, ispif->irq, ispif_isr_8x16,
 			       IRQF_TRIGGER_RISING, ispif->irq_name, ispif);
-	else if (camss->version == CAMSS_8x96 ||
+	else if (camss->version == CAMSS_8x17 ||
+		 camss->version == CAMSS_8x96 ||
 		 camss->version == CAMSS_660)
 		ret = devm_request_irq(dev, ispif->irq, ispif_isr_8x96,
 			       IRQF_TRIGGER_RISING, ispif->irq_name, ispif);
