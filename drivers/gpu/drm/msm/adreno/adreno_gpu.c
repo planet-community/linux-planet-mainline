36ee781a4450df79171d7d8202f008ee798ffb07
Otto Pflüger <otto.pflueger@abscue.de>
2023-04-20 17:54:16 +0200

    WIP: drm/msm: Adreno A308 support
    Signed-off-by: Otto Pflüger <otto.pflueger@abscue.de>

--- drivers/gpu/drm/msm/adreno/adreno_gpu.c
+++ drivers/gpu/drm/msm/adreno/adreno_gpu.c
@@ -318,6 +318,9 @@ int adreno_get_param(struct msm_gpu *gpu, struct msm_file_private *ctx,
 	switch (param) {
 	case MSM_PARAM_GPU_ID:
 		*value = adreno_gpu->info->revn;
+		if (*value == 308) {
+			*value = 307;
+		}
 		return 0;
 	case MSM_PARAM_GMEM_SIZE:
 		*value = adreno_gpu->gmem;
