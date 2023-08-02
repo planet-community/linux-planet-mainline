370faf302789c8f7362df04f117886f214ed2f29
Otto Pflüger <otto.pflueger@abscue.de>
2023-08-02 17:48:45 +0200

    HACK: drm/msm: always use IOMMU on all platforms

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -305,14 +305,7 @@ bool msm_use_mmu(struct drm_device *dev)
 {
 	struct msm_drm_private *priv = dev->dev_private;
 
-	/*
-	 * a2xx comes with its own MMU
-	 * On other platforms IOMMU can be declared specified either for the
-	 * MDP/DPU device or for its parent, MDSS device.
-	 */
-	return priv->is_a2xx ||
-		device_iommu_mapped(dev->dev) ||
-		device_iommu_mapped(dev->dev->parent);
+	return true;
 }
 
 static int msm_init_vram(struct drm_device *dev)
